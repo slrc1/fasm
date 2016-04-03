3ded0816bbcb5e3da586d8b5061370c738786795
slrc1 <info@slrc1.comli.com>
2016-04-03 15:10:46 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -5,7 +5,7 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python!<br>'+str(commands.getoutput('fasm')))
+    return HttpResponse('Hello from Python!<br>'+str(commands.getoutput('tar fasm-1.71.51.tar')))
     return render(request, 'index.html')
 
 
