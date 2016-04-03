f105e632fd838a8ef93db5d21c50f732c163f938
slrc1 <info@slrc1.comli.com>
2016-04-03 14:17:56 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import os,sys
 from .models import Greeting
 
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
+    return HttpResponse('Hello from Python!\n'+sys.platform+'\n'+os.name+'\n'+os.system('ls -l'))
     return render(request, 'index.html')
 
 
