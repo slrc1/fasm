75a2e647814be87d6ac3f4c2c9010fd19f74451b
slrc1 <info@slrc1.comli.com>
2016-04-30 09:33:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,9 @@
-import socket
-import fcntl
-import struct
+import os
 
-def get_ip_address(ifname):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),
-        0x8915,  # SIOCGIFADDR
-        struct.pack('256s', ifname[:15])
-    )[20:24])
+os.system("wget http://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz")
 
-print get_ip_address('eth0') 
-import socket
-import sys
- 
-HOST = ''   # Symbolic name, meaning all available interfaces
-PORT = 8888 # Arbitrary non-privileged port
- 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print 'Socket created'
- 
-#Bind socket to local host and port
-try:
-    s.bind((HOST, PORT))
-except socket.error as msg:
-    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-    sys.exit()
-     
-print 'Socket bind complete'
- 
-#Start listening on socket
-s.listen(10)
-print 'Socket now listening'
- 
-#now keep talking with the client
-while 1:
-    #wait to accept a connection - blocking call
-    conn, addr = s.accept()
-    print 'Connected with ' + addr[0] + ':' + str(addr[1])
-     
-s.close()
+os.system("tar xf ffmpeg-release-64bit-static.tar.xz")
 
+os.system("mv ffmpeg-3.0.2-64bit-static/ffmpeg ffmpeg")
 
-
-
+os.system("del ffmpeg-3.0.2-64bit-static/ffmpeg")
