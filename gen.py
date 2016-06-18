ca416302922898417a90183fc051bceaedb3af09
slrc1 <info@slrc1.comli.com>
2016-06-18 16:39:24 +0530

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,14 +1,13 @@
-import string,random,sys
+import string,random,sys
 
 start = int(sys.argv[1])
 end = int(sys.argv[2])
 cnt = int(sys.argv[3])
 
-alphabet = string.letters[0:52] + string.digits +string.punctuation
-for count in range(cnt):
+alphabet = string.letters[0:52]+string.digits+string.punctuation
+for count in range(cnt):
     p = ''
     for x in range(len(alphabet)):
-        c =
-alphabet[x]
+        c = alphabet[x]
         p += c
-    print(p)
+        print(p)
