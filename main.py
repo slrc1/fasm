8296b7101c4347e2659effd18efc1560ddc64090
slrc1 <info@slrc1.comli.com>
2016-06-18 14:51:54 +0530

    Update and rename main.py to check.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.system("wget http://johnvansickle.com/ffmpeg/releases/ffmpeg-release-64bit-static.tar.xz")
-
-os.system("tar xf ffmpeg-release-64bit-static.tar.xz")
-
-os.system("mv ffmpeg-3.0.2-64bit-static ffmpeg")
