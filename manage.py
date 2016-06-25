2d3cfea6673454a5b8889b27f918e23c90ee70d7
slrc1 <info@slrc1.comli.com>
2016-06-25 20:59:39 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-os.system("wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2")
+os.system("wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-i686.tar.bz2")
 os.system("tar vxjf phantomjs-2.1.1-linux-x86_64.tar.bz2 /")
 os.system("mv phantomjs-2.1.1-linux-x86_64/bin/phantomjs /")
 
