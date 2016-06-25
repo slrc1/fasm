2f53a205a3b092fc028124c4d651c33796e769d2
slrc1 <info@slrc1.comli.com>
2016-06-25 23:16:02 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,8 +3,8 @@ import os
 import sys
 
 os.system("wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-i686.tar.bz2")
-os.system("tar vxjf phantomjs-2.1.1-linux-x86_64.tar.bz2 /")
-os.system("mv phantomjs-2.1.1-linux-x86_64/bin/phantomjs /")
+os.system("tar vxjf phantomjs-2.1.1-linux-i686.tar.bz2 /")
+os.system("mv phantomjs-2.1.1-linux-i686/bin/phantomjs /")
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
